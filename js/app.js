c89462fe045143d0199ee3954c06910c549b6fc3
dimd13 <dimd.wolf13@gmail.com>
2015-03-08 10:56:26 +0500

    one request get all

--- js/app.js
+++ js/app.js
@@ -1,34 +1,26 @@
 var app = angular.module('app', []);
-app.controller('svgMap', function($scope, $rootScope) {
-  $scope.countries = {
-    'russia': {
-      name: 'Россия'
-  }}
+app.controller('svgMap', function($scope, $rootScope, $http) {
 
+  $http.get('data/countries.json').success(function(data) {
+    $scope.countries = data;
+  });
 
 });
-app.directive('svgMap', function($rootScope,$http,$timeout) {
+app.directive('svgMap', function($rootScope) {
   return {
     restrict: 'A',
     templateUrl: 'img/map.svg',
     link: function(scope, element, attrs) {
       element.find('path').bind('click', function() {
-      scope.country = {};
-        $http.get('data/'+this.id+'.json').success(function(data) {
-          scope.country = data;
+        scope.country = {};
+        scope.country = scope.countries[this.id];
+        scope.$digest();
+        angular.element('body').addClass('open-modal');
+        angular.element('.full-width-modal').removeClass('closed').addClass('opened');
+        angular.element('div.__close-modal').bind('click', function () {
+          angular.element('.full-width-modal').removeClass('opened').addClass('closed');
+          angular.element('body').removeClass('open-modal');
         });
-          //$timeout(function () {
-            scope.$digest();
-            angular.element('body').addClass('open-modal');
-            angular.element('.full-width-modal').removeClass('closed').addClass('opened');
-            angular.element('div.__close-modal').bind('click', function () {
-              angular.element('.full-width-modal').removeClass('opened').addClass('closed');
-              angular.element('body').removeClass('open-modal');
-            })
-          //},0)
-
-
-        //console.log(this.id)
       });
     }
   }
