61b524f91446e28911d3eeb5f6a686506031f70d
dimd13 <dimd.wolf13@gmail.com>
2015-03-09 19:26:55 +0500

    up

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
 var app = angular.module('app', []);
 app.controller('svgMap', function($scope, $rootScope, $http) {
-
+  $scope.countries = {};
   $http.get('data/countries.json').success(function(data) {
     $scope.countries = data;
+    angular.element('svg').find('path').each(function(i, el){
+      var id = el.id
+      if ($scope.countries.hasOwnProperty(id)) {
+        //d3.selectAll('#'+ id).attr('data-content', '<h3><img src="'+$scope.countries[id]['flag']+'" alt=""/>'+$scope.countries[id]['name']+'</h3>');
+        d3.selectAll('#'+ id).attr('data-content', $scope.countries[id]['text']);
+        d3.selectAll('#'+ id).attr('title', '<div class="popover-country"><img src="'+$scope.countries[id]['flag']+'" alt=""/>'+$scope.countries[id]['name']+'</div>');
+      }
+    });
+    $('svg .country_shape').popover({
+      'trigger':'hover'
+      ,'container': 'body'
+      ,'placement': 'auto'
+      ,'white-space': 'nowrap'
+      ,'html':'true'
+    });
+
   });
 
 });
-app.directive('svgMap', function($rootScope) {
+app.directive('svgMap', function() {
   return {
     restrict: 'A',
     templateUrl: 'img/map.svg',
