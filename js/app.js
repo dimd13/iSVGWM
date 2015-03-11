4222c2677aaf6181168875aed9c80984913fb275
dimd13 <dimd.wolf13@gmail.com>
2015-03-11 18:02:23 +0500

    then

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 var app = angular.module('app', []);
-app.controller('svgMap', function($scope, $rootScope, $http) {
+app.controller('svgMapData', function($scope, $rootScope, $http) {
   $scope.countries = {};
   $http.get('data/countries.json').success(function(data) {
-    $scope.countries = data;
+    return $scope.countries = data;
 
   }).then(function() {
     /*TODO: some times not work...WHY?*/
-    jQuery('svg').find('path').each(function(i, el){
-    console.log('asd')
-      var id = el.id;
-      console.log(id)
-      if ($scope.countries.hasOwnProperty(id)) {
-        //d3.selectAll('#'+ id).attr('data-content', '<h3><img src="'+$scope.countries[id]['flag']+'" alt=""/>'+$scope.countries[id]['name']+'</h3>');
-        d3.selectAll('#'+ id).attr('data-content', $scope.countries[id]['text']);
-        d3.selectAll('#'+ id).attr('title', '<div class="popover-country"><img src="'+$scope.countries[id]['flag']+'" alt=""/>'+$scope.countries[id]['name']+'</div>');
+
+    jQuery('.country_shape').each(function(){
+      if ($scope.countries.hasOwnProperty(this.id)) {
+        d3.select(this)
+          .attr('data-toggle','popover')
+          .attr('data-content', $scope.countries[this.id]['text'])
+          .attr('title', '<div class="popover-country"><img src="'+$scope.countries[this.id]['flag']+'" alt=""/>'+$scope.countries[this.id]['name']+'</div>');
+        angular.element(this).popover({
+          'trigger':'hover'
+          ,'container': 'body'
+          ,'placement': 'auto'
+          ,'white-space': 'wrap'
+          ,'html':'true'
+        })
       }
     });
 
@@ -35,13 +41,6 @@ app.controller('svgMap', function($scope, $rootScope, $http) {
         angular.element('body').removeClass('open-modal');
       });
     };
-    angular.element('svg path.country_shape').popover({
-      'trigger':'hover'
-      ,'container': '#map'//body
-      ,'placement': 'auto'
-      ,'white-space': 'wrap'
-      ,'html':'true'
-    });
   });
 
 });
@@ -53,9 +52,10 @@ app.directive('svgMap', function() {
       element.find('path').bind('click', function() {
         if (scope.countries.hasOwnProperty(this.id)) {
           scope.showCountryInfo(this.id);
-          scope.$digest();
         }
+        scope.$digest();
       });
-    }
+    },
+    controller: 'svgMapData'
   }
 });
